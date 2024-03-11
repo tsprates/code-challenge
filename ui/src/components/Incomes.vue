f39855035ee6381f8ffd2dc29337b5cd05d075b3
Thiago Prates <tsprates@gmail.com>
2024-03-11 11:07:37 -0300

    list checks by status

--- ui/src/components/Incomes.vue
+++ ui/src/components/Incomes.vue
@@ -1,15 +1,21 @@
 <template>
     <Toolbar title="Expenses" classes="bg-blue-100 text-blue-400" />
-    
+
     <Date />
-    
+
+    <ul class="list-status">
+        <li :class="{ 'status': true, 'active-status': status === 'pending' }" @click="status = 'pending'">Pending</li>
+        <li :class="{ 'status': true, 'active-status': status === 'accepted' }" @click="status = 'accepted'">Accepted</li>
+        <li :class="{ 'status': true, 'active-status': status === 'rejected' }" @click="status = 'rejected'">Rejected</li>
+    </ul>
+
     <Loading v-if="!isLoaded" />
 
-    <ListTransactions v-if="isLoaded" :transactions="transactions" />
+    <ListTransactions v-if="isLoaded" :transactions="currentIncomes" />
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import http from '../services/http';
 import Date from './Date.vue';
 import ListTransactions from './ListTransactions.vue';
@@ -19,17 +25,23 @@ import Toolbar from './Toolbar.vue';
 const isLoaded = ref(false)
 
 const transactions = ref([])
+const currentIncomes = ref([])
+const status = ref('accepted')
 
 onMounted(() => {
     http().get('/incomes')
         .then((response) => {
             transactions.value = response.data
-            console.log(transactions.value)
+            currentIncomes.value = transactions.value.filter((income) => (income.check.status === status.value))
         })
         .catch((error) => {
             console.log(error)
         })
         .finally(() => (isLoaded.value = true))
+})
+
+watch(status, (newStatus) => {
+    currentIncomes.value = transactions.value.filter((income) => (income.check.status === newStatus))
 })
 
 </script>
