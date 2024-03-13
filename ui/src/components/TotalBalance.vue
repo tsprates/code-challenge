f94390c5d78dfee5ab6b08546b7537be125bdbec
Thiago Prates <tsprates@gmail.com>
2024-03-13 14:47:41 -0300

    refactoring balance component

--- ui/src/components/TotalBalance.vue
+++ ui/src/components/TotalBalance.vue
@@ -3,17 +3,17 @@
         <div class="bg-blue-300 text-white pt-8 pb-4 px-5 flex flex-row items-end justify-between">
             <div class="flex flex-col items-center justify-between">
                 <h2 class="font-semibold text-sm">Current Balance</h2>
-                <span v-if="current_balance >= 0" class="text-4xl font-semibold">${{ current_balance }}</span>
-                <span v-else class="text-4xl font-semibold text-red-400">-${{ Math.abs(current_balance) }}</span>
+                <span v-if="current_balance >= 0" class="text-4xl font-semibold">${{ currentBalance }}</span>
+                <span v-else class="text-4xl font-semibold text-red-400">-${{ currentBalance }}</span>
             </div>
-            <div class="font-semibold text-lg w-1/3 md:text-center">{{ moment().format('MMMM, YYYY') }}</div>
+            <div class="font-semibold text-lg w-1/3 md:text-center">{{ currentDate }}</div>
         </div>
 
         <div class="bg-blue-200 text-blue-500 px-4 py-6">
             <div class="flex flex-row items-center justify-between">
                 <span>
                     <h4 class="font-semibold text-sm">Income</h4>
-                    <span class="text-2xl font-semibold">${{ total_income }}</span>
+                    <span class="text-2xl font-semibold">${{ totalIncome }}</span>
                 </span>
                 <div class="flex flex-col items-center justify-center w-1/3 active:text-blue-500/50 hover:cursor-pointer"
                     @click="goToDepositCheck">
@@ -30,7 +30,7 @@
             <div class="flex flex-row items-center justify-between">
                 <span>
                     <h4 class="font-semibold text-sm">Expense</h4>
-                    <span class="text-2xl font-semibold">${{ total_expense }}</span>
+                    <span class="text-2xl font-semibold">${{ totalExpense }}</span>
                 </span>
                 <div class="flex flex-col items-center justify-center w-1/3 active:text-blue-500/50 hover:cursor-pointer"
                     @click="goToAddPurchase">
@@ -47,18 +47,19 @@
 
 <script setup>
 import moment from 'moment';
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter()
 
 const { current_balance, total_income, total_expense } = defineProps(['current_balance', 'total_income', 'total_expense'])
 
-const goToAddPurchase = () => {
-    router.push({ name: 'addPurchase' })
-}
+const currentBalance = ref(parseFloat(current_balance).toFixed(2))
+const totalIncome = ref(parseFloat(total_income).toFixed(2))
+const totalExpense = ref(parseFloat(total_expense).toFixed(2))
 
-const goToDepositCheck = () => {
-    router.push({ name: 'depositCheck' })
-}
+const currentDate = moment().format('MMMM, YYYY')
+
+const goToAddPurchase = () => router.push({ name: 'addPurchase' })
+const goToDepositCheck = () => router.push({ name: 'depositCheck' })
 </script>
